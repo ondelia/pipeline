c9187dafb2cf70da7a4c342d077239b5ca6ab002
ondelia <noobsensei@hotmail.com>
2021-06-22 22:53:19 +0200

    Added functions to download front page of Reddit and parse the headlines.

--- downloadpage.py
+++ downloadpage.py
@@ -1,6 +1,34 @@
 import urllib.request, urllib.error, urllib.parse
+import re
 
 def getContent(url):
     response = urllib.request.urlopen(url)
     webContent = response.read()
-    return(webContent)
+    return(str(webContent))
+
+def trimText(text):
+    trimmedText = text[text.find('"reddit: the front page of the internet"}'):]
+    trimmedText = trimmedText[:trimmedText.find('"Popular Communities"')]
+
+    trimmedText = re.sub(r'"title":', r'"title":\n', trimmedText)
+    trimmedText = re.sub(r'","upvoteRatio.*?"title":', r'', trimmedText)
+    trimmedText = re.sub(r'"reddit: the front page of the internet"}.*?"title":', r'', trimmedText)
+    trimmedText = re.sub(r',"topAwardedType":"ACTIVE', r'', trimmedText)
+    trimmedText = re.sub(r',"topAwardedType":"ACTIVE', r'', trimmedText)
+
+    trimmedText = re.sub(r'"\n', r'\n', trimmedText)
+    trimmedText = re.sub(r'\n"', r'\n', trimmedText)
+
+    return trimmedText
+
+def sepHeadlines(text):
+    text = text.split('\n')
+
+    newText = []
+
+    for element in text:
+        if element != '' and element not in newText:
+            newText.append(element)
+
+    return newText
+
--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+from downloadpage import *
+
 print("Hello world!")
+
+#1. The Python script will download the front page of Reddit.
+fulltext = getContent('http://www.reddit.com')
+
+#2. The script will parse the JSON, HTML, or CSV file to separate the headline and the subreddit on which it appeared. It will clean the data, add a time stamp column, and add a source column that says "Reddit".
+trimmedtext = trimText(fulltext)
+
+with open('reddit.txt', 'w') as f:
+    f.write('')
+
+headlines = sepHeadlines(trimmedtext)
+print("Separated Headlines")
+for element in headlines:
+    print(element)
+    with open('reddit.txt', 'a') as f:
+        f.write(element + '\n')
+
+
+
+#3. On AWS, there will be a database and table called Headline to load this data. There will also be a table called Wordcount to count the words in the headlines.
+#4. The Python script will access the database using environmental variables, and insert a row into the Headline table for each headline.
+#5. The Python script will increment the words in the Wordcount for each word in a headline.
+#6. The Python script will store the data file locally and on S3.
