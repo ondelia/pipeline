81c968af0d6a7f1de0988cbd7d22f7ac6f92e162
ondelia <noobsensei@hotmail.com>
2021-06-23 11:26:11 +0200

    Added subreddit as well as the headline.

--- downloadpage.py
+++ downloadpage.py
@@ -10,10 +10,14 @@ def trimText(text):
     trimmedText = text[text.find('"reddit: the front page of the internet"}'):]
     trimmedText = trimmedText[:trimmedText.find('"Popular Communities"')]
 
-    trimmedText = re.sub(r'"title":', r'"title":\n', trimmedText)
-    trimmedText = re.sub(r'","upvoteRatio.*?"title":', r'', trimmedText)
-    trimmedText = re.sub(r'"reddit: the front page of the internet"}.*?"title":', r'', trimmedText)
-    trimmedText = re.sub(r',"topAwardedType":"ACTIVE', r'', trimmedText)
+    trimmedText = re.sub(r'","upvoteRatio.*?https://www.reddit.com/r/', r'\n', trimmedText)
+    trimmedText = re.sub(r'","upvoteRatio.*?$', r'\n', trimmedText)
+
+    trimmedText = re.sub(r'"reddit: the front page of the internet"}.*?https://www.reddit.com/r/', r'\n', trimmedText)
+
+    trimmedText = re.sub(r'/comments/.*?"title":', r'\n', trimmedText)
+
+    trimmedText = re.sub(r'"title":', r'"title":\n\n', trimmedText)
     trimmedText = re.sub(r',"topAwardedType":"ACTIVE', r'', trimmedText)
 
     trimmedText = re.sub(r'"\n', r'\n', trimmedText)
--- main.py
+++ main.py
@@ -12,13 +12,10 @@ with open('reddit.txt', 'w') as f:
     f.write('')
 
 headlines = sepHeadlines(trimmedtext)
-print("Separated Headlines")
 for element in headlines:
     print(element)
     with open('reddit.txt', 'a') as f:
         f.write(element + '\n')
-
-
 
 #3. On AWS, there will be a database and table called Headline to load this data. There will also be a table called Wordcount to count the words in the headlines.
 #4. The Python script will access the database using environmental variables, and insert a row into the Headline table for each headline.
