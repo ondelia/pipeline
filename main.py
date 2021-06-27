322bf79dd49bd8d1f19f9b7436b2d1a8d7d6ba12
ondelia <noobsensei@hotmail.com>
2021-06-27 14:05:26 +0200

    Tested database connection.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from downloadpage import *
+from mysqldb import *
+from loadinfo import *
 
 print("Hello world!")
 
@@ -19,5 +21,11 @@ for element in headlines:
 
 #3. On AWS, there will be a database and table called Headline to load this data. There will also be a table called Wordcount to count the words in the headlines.
 #4. The Python script will access the database using environmental variables, and insert a row into the Headline table for each headline.
+
+host, user, password = loadinfo()
+mydb, mycursor = connectToDatabase(host, user, password, buffered=True)
+
+
+
 #5. The Python script will increment the words in the Wordcount for each word in a headline.
 #6. The Python script will store the data file locally and on S3.
