fff4222deca464fa762416912c6b61e258161155
ondelia <noobsensei@hotmail.com>
2021-06-29 21:16:23 +0200

    Insert CSV file into the database.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from downloadpage import *
 from mysqldb import *
 from loadinfo import *
+from datetime import datetime
+from shutil import copyfile
 
 print("Hello world!")
+host, user, password, source, destination = loadinfo()
 
 #1. The Python script will download the front page of Reddit.
 fulltext = getContent('http://www.reddit.com')
@@ -10,22 +13,27 @@ fulltext = getContent('http://www.reddit.com')
 #2. The script will parse the JSON, HTML, or CSV file to separate the headline and the subreddit on which it appeared. It will clean the data, add a time stamp column, and add a source column that says "Reddit".
 trimmedtext = trimText(fulltext)
 
-with open('reddit.txt', 'w') as f:
+with open('reddit.csv', 'w') as f:
     f.write('')
 
 headlines = sepHeadlines(trimmedtext)
+now = datetime.utcnow()
+myTime = now.strftime("%Y-%m-%d-%H%M%S")
+
 for element in headlines:
     print(element)
-    with open('reddit.txt', 'a') as f:
-        f.write(element + '\n')
+    with open('reddit.csv', 'a') as f:
+        f.write(element + '||||Reddit||||' + myTime + '\n')
 
 #3. On AWS, there will be a database and table called Headline to load this data. There will also be a table called Wordcount to count the words in the headlines.
 #4. The Python script will access the database using environmental variables, and insert a row into the Headline table for each headline.
 
-host, user, password = loadinfo()
-mydb, mycursor = connectToDatabase(host, user, password, buffered=True)
+mydb, mycursor = connectToDatabase(host, user, password)
 
+destination = destination.replace('TEST.csv', myTime + '.csv')
+copyfile(source, destination)
 
+insertNewHeadlines(mydb, mycursor, destination)
 
 #5. The Python script will increment the words in the Wordcount for each word in a headline.
 #6. The Python script will store the data file locally and on S3.
--- mysqldb.py
+++ mysqldb.py
@@ -1,6 +1,6 @@
 import mysql.connector as sq
 
-def connectToDatabase(host, user, passwd, buffered):
+def connectToDatabase(host, user, passwd, buffered=True):
     mydb = sq.connect(host=host, user=user, passwd=passwd, buffered=buffered)
     mycursor = mydb.cursor()
 
@@ -8,13 +8,34 @@ def connectToDatabase(host, user, passwd, buffered):
     for db in mycursor:
         print(db)
 
-    mycursor.execute("SELECT COUNT(*) FROM frontpage.Headlines")
+    mycursor.execute("USE frontpage")
+
+    return mydb, mycursor
+
+def insertNewHeadlines(mydb, mycursor, csvFile):
+    # File MUST be stored locally with the database, or the script may not work properly.
+    csvFile = csvFile.replace('\\', '/')
+    mycursor.execute("SHOW VARIABLES LIKE 'secure_file_priv';")
     result = mycursor.fetchall()
     print(result)
 
-    mycursor.execute("select * from frontpage.Headlines")
+    insertCommand = "LOAD DATA INFILE '" + csvFile + "' \
+    INTO TABLE headlines \
+    FIELDS TERMINATED BY '||||' \
+    LINES TERMINATED BY '\\r\\n' \
+    (category,headline,datasource,scraped);"
+
+    print(insertCommand)
+    mycursor.execute(insertCommand)
+
+    mycursor.execute("SELECT * FROM frontpage.headlines")
     result = mycursor.fetchall()
     for row in result:
         print(row)
 
-    return mydb, mycursor
+    print("COUNT AFTER INSERT:")
+    mycursor.execute("SELECT COUNT(*) FROM frontpage.headlines")
+    result = mycursor.fetchall()
+    print(result)
+
+    mydb.commit()
