3827939542c42225fa2e57bf1c610d43c16b7a6c
ondelia <noobsensei@hotmail.com>
2021-06-30 12:55:22 +0200

    Created wordcount function.

--- main.py
+++ main.py
@@ -19,11 +19,22 @@ with open('reddit.csv', 'w') as f:
 headlines = sepHeadlines(trimmedtext)
 now = datetime.utcnow()
 myTime = now.strftime("%Y-%m-%d-%H%M%S")
+headlinesOnly = ''
 
 for element in headlines:
     print(element)
     with open('reddit.csv', 'a') as f:
         f.write(element + '||||Reddit||||' + myTime + '\n')
+
+    element = element[element.find('||||'):]
+    element = element[4:]
+
+    headlinesOnly = headlinesOnly + ' ' + element
+    print(element)
+
+headlinesOnly = headlinesOnly.lower()
+headlinesOnly = "".join([ch for ch in headlinesOnly if ch in 'abcdefghijklmnopqrstuvwxyz '])
+print(headlinesOnly)
 
 #3. On AWS, there will be a database and table called Headline to load this data. There will also be a table called Wordcount to count the words in the headlines.
 #4. The Python script will access the database using environmental variables, and insert a row into the Headline table for each headline.
@@ -36,4 +47,7 @@ copyfile(source, destination)
 insertNewHeadlines(mydb, mycursor, destination)
 
 #5. The Python script will increment the words in the Wordcount for each word in a headline.
+updateWordcount(mydb, mycursor, headlinesOnly, myTime)
+
+
 #6. The Python script will store the data file locally and on S3.
--- mysqldb.py
+++ mysqldb.py
@@ -1,4 +1,5 @@
 import mysql.connector as sq
+import csv
 
 def connectToDatabase(host, user, passwd, buffered=True):
     mydb = sq.connect(host=host, user=user, passwd=passwd, buffered=buffered)
@@ -28,14 +29,34 @@ def insertNewHeadlines(mydb, mycursor, csvFile):
     print(insertCommand)
     mycursor.execute(insertCommand)
 
-    mycursor.execute("SELECT * FROM frontpage.headlines")
+    mycursor.execute("SELECT * FROM headlines")
     result = mycursor.fetchall()
     for row in result:
         print(row)
 
     print("COUNT AFTER INSERT:")
-    mycursor.execute("SELECT COUNT(*) FROM frontpage.headlines")
+    mycursor.execute("SELECT COUNT(*) FROM headlines")
     result = mycursor.fetchall()
     print(result)
 
     mydb.commit()
+
+def updateWordcount(mydb, mycursor, headlinesOnly, myTime):
+    print("UPDATING WORD COUNT")
+    print(headlinesOnly)
+
+    words = headlinesOnly.split()
+    print(words)
+
+    for word in words:
+        mycursor.execute("SELECT * FROM wordcount WHERE word='" + word + "';")
+        result = mycursor.fetchall()
+        print(result)
+
+        if result:
+            updateStatement = "UPDATE wordcount SET wordcount = wordcount + 1 WHERE word='" + word + "';"
+        else:
+            updateStatement = "INSERT INTO wordcount VALUES ('" + word + "', 1, 'Reddit', '" + myTime + "')"
+
+        mycursor.execute(updateStatement)
+        mydb.commit()
